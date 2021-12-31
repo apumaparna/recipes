49fa367fa43f4c6a071b5d82fc4e7cc9e7e7b55d
apumaparna <aparnakumargs@gmail.com>
2021-12-31 18:18:25 -0500

    display recipe names

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // import 'dart:html';
+import 'recipe.dart';
 
 import 'package:flutter/material.dart';
 
@@ -41,10 +42,14 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: SafeArea(
-        // Having a SafeArea prevents the app contents from
-        // encroaching on operating system interfaces
-        child: Container(),
-      ),
+          // Having a SafeArea prevents the app contents from
+          // encroaching on operating system interfaces
+          child: ListView.builder(
+        itemCount: Recipe.samples.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Text(Recipe.samples[index].label);
+        },
+      )),
     );
   }
 }
