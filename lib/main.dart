b09dd1b245ee7d59ad57cabf3be8ffa24ad2fbc7
apumaparna <aparnakumargs@gmail.com>
2022-01-01 23:33:32 -0500

    formatting

--- lib/main.dart
+++ lib/main.dart
@@ -54,10 +54,25 @@ class _MyHomePageState extends State<MyHomePage> {
   // Add buildRecipeCard() here
   Widget buildRecipeCard(Recipe recipe) {
     return Card(
-      child: Column(children: <Widget>[
-        Image(image: AssetImage(recipe.imageUrl)),
-        Text(recipe.label),
-      ]),
-    );
+        // elevation adds shadow
+        elevation: 2.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Palatino'),
+            ),
+          ]),
+        ));
   }
 }
